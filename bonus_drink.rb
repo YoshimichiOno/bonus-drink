ad147e91c6daaf9912e73ea9f9267010a39a5ac9
Yoshihide.Ono <yono1961@gmail.com>
2015-07-06 01:12:53 +0900

    First commit

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,27 @@
 class BonusDrink
+  EXCHANGE = 3
+
   def self.total_count_for(amount)
-    0
+    drinked = 0
+    drinkable = amount
+    loop {
+      present = drinkable.divmod(EXCHANGE)
+      drinked += present[0] * EXCHANGE 
+      drinkable = present[0] + present[1]
+      if (drinkable < EXCHANGE) then
+        drinked += drinkable
+        break
+      end
+    }
+    return drinked
   end
 end
+
+=begin
+  print "購入数：0、飲める本数：",BonusDrink.total_count_for(0),"\n"
+  print "購入数：1、飲める本数：",BonusDrink.total_count_for(1),"\n"
+  print "購入数：3、飲める本数：",BonusDrink.total_count_for(3),"\n"
+  print "購入数：11、飲める本数：",BonusDrink.total_count_for(11),"\n"
+  print "購入数：100、飲める本数：",BonusDrink.total_count_for(100),"\n"
+  print "購入数：10000000、飲める本数：",BonusDrink.total_count_for(10000000),"\n"
+=end 
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  context '購入本数＝０本' do
+    specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  end
+  context '購入本数＜空瓶３本' do
+    specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+  end
+  context '購入本数＝空瓶３本' do
+    specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  end
+  context '購入本数＞空瓶３本' do
+    specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+    specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+  end
 end
